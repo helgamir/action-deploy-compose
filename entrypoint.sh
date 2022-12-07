fddb90e8ff67238732e1241b87d921b0e3020931
Ameer Hamza Naveed <ameerhmzx@gmail.com>
2022-12-07 10:33:07 +0500

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -57,8 +57,11 @@ then
   cd \$workdir; 
 
   if $NO_CACHE
-  then docker-compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --remove-orphans --force-recreate
-  else docker-compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --remove-orphans --build;
+  then
+    docker-compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" build --no-cache 
+    docker-compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --remove-orphans --force-recreate
+  else 
+    docker-compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --remove-orphans --build;
   fi
 fi"
 
